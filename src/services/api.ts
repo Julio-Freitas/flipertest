4ef1ae7c19e2d652a05cc375c77bbd87e6004ecb
Julio-Freitas <julio.clf31@gmail.com>
2021-07-06 08:20:25 -0300

    FIX: adjust file api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,10 @@
-import ApolloClient from 'apollo-boost';
+import ApolloClient, { InMemoryCache } from 'apollo-boost';
 
 const client = new ApolloClient({
     uri: 'https://harura-fliper-test.herokuapp.com/v1/graphql',
     headers: {
         'x-hasura-admin-secret': 'fliperdevtest2020',
     },
+    cache: new InMemoryCache(),
 });
 export default client;
